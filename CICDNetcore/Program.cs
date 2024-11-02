781a8d47b36526794b7a08190150fdf057bdcd56
chivy140820a <chivy1408@gmail.com>
2024-11-02 13:38:45 +0700

    update

--- CICDNetcore/Program.cs
+++ CICDNetcore/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
+using Microsoft.AspNetCore.DataProtection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo("C:\\keys")).ProtectKeysWithDpapi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
