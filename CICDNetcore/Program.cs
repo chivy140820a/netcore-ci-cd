d5398a6b94eaa4e4ca3ccbb1183ae50909339178
chivy140820a <chivy1408@gmail.com>
2024-11-05 21:41:27 +0700

    update

--- CICDNetcore/Program.cs
+++ CICDNetcore/Program.cs
@@ -1,18 +1,21 @@
-using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
+﻿using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption;
 using Microsoft.AspNetCore.DataProtection;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 var directoryPath = @"/app/keys";
+var certPath = @"/app/certs/your-certificate.pfx";
+var certPassword = "your-password"; // Mật khẩu bảo vệ chứng chỉ
 builder.Services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(directoryPath))
                 .UseCryptographicAlgorithms(new AuthenticatedEncryptorConfiguration()
                 {
                     EncryptionAlgorithm = EncryptionAlgorithm.AES_256_CBC,
                     ValidationAlgorithm = ValidationAlgorithm.HMACSHA256
-                });
+                }).ProtectKeysWithCertificate(new X509Certificate2(certPath, certPassword));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
